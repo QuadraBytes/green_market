f8ec84d1e75acda4fe11a076ce6d91c3bd2332bc
Imesh Thanapathi <imeshthanapathi@gmail.com>
2024-07-24 23:48:26 +0530

    bottom sheets added-filtering and crop detail UI

---
+++ lib/models/models.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+const districts = [
+  'Ampara',
+  'Anuradhapura',
+  'Badulla',
+  'Batticaloa',
+  'Colombo',
+  'Galle',
+  'Gampaha',
+  'Hambantota',
+  'Jaffna',
+  'Kalutara',
+  'Kandy',
+  'Kegalle',
+  'Kilinochchi',
+  'Kurunegala',
+  'Mannar',
+  'Matale',
+  'Matara',
+  'Monaragala',
+  'Mullaitivu',
+  'Nuwara Eliya',
+  'Polonnaruwa',
+  'Puttalam',
+  'Ratnapura',
+  'Trincomalee',
+  'Vavuniya'
+];
+
+const priceRange = [
+  100, 
+  200, 
+  300, 
+  400, 
+  500, 
+  600
+];
+
+const weightRange = [
+  1,
+  2, 
+  3, 
+  4, 
+  5, 
+  6
+];
+
+
